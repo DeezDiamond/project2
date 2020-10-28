22ac0c0d73dee919d067798357bb057b19642a82
Dee <71417512+DeezDiamond@users.noreply.github.com>
2020-10-28 10:23:46 -0600

    Various updates
    Started messing with CSS, deleted unnecessary files, updated README.md, tweaked code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
+	<Router>
+		<App />
+	</Router>,
+	document.getElementById('root')
 );
