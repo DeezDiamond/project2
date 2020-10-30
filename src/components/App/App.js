004219f772fa97832265bee9b660ef539be3d993
Dee <71417512+DeezDiamond@users.noreply.github.com>
2020-10-30 10:12:57 -0600

    Added more components
    Added species pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Header from '../Header/Header';
 import Movies from '../Movies/Movies';
 import Movie from '../Movie/Movie';
+import Species from "../Species/Species"
+import Specie from "../Specie/Specie"
 
 function App() {
 	return (
 		<div className='App'>
 			<Header />
 			<main>
-				<Route path='/' exact component={Movies} />
+				<Switch>
+				<Route path='/' exact component={Movies} /> 
+
 				<Route
 					path='/films/:id'
 					render={(routerProps) => {
 						return <Movie match={routerProps.match} />;
 					}}
 				/>
+
+				<Route path="/species" exact component={Species} />
+
+				<Route 
+				path="/species/:id"
+				render={(routerProps) => {
+					return <Specie match={routerProps.match} />
+				}} />
+
+				</Switch>
 			</main>
 		</div>
 	);
